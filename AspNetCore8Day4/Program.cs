013deea86d092d2acb5c121719b1d94cff421cde
Will <doggy.huang@gmail.com>
2024-06-01 14:27:29 +0800

    示範一種特殊的 EF Core DbContext 用法

--- AspNetCore8Day4/Program.cs
+++ AspNetCore8Day4/Program.cs
@@ -2,8 +2,26 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddDbContext<ContosoUniversityContext>(
-    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//builder.Services.AddDbContext<ContosoUniversityContext>(
+//    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped(sp =>
+{
+    var conn = builder.Configuration.GetConnectionString("DefaultConnection")!;
+
+    var httpContextAccessor = sp.GetRequiredService<IHttpContextAccessor>();
+
+    httpContextAccessor.HttpContext!.Request.Headers.TryGetValue("X-Database-Name", out var dbname);
+
+    conn = conn.Replace("ABC", dbname);
+
+    return new ContosoUniversityContext(
+            new DbContextOptionsBuilder<ContosoUniversityContext>()
+                .UseSqlServer(conn).Options
+        );
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
